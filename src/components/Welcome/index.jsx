5e2a3de5258f0af32eb6eb639fb8d6b19e765092
Wesleyxl <wesley.alvesxll@gmail.com>
2022-03-23 13:29:48 -0300

    started

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function Welcome() {
+  return (
+    <Container>
+      <h3>Hi, Welcome to React JS</h3>
+    </Container>
+  );
+}
+
+export default Welcome;
